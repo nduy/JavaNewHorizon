072a4f56541e775c545fa3c7c025d69ddae7e13b
duyndx <duyndx@gmail.com>
2025-02-19 09:13:15 +0000

    Print date time then a triangle with customised number of row

--- JavaNewHorizon.java
+++ JavaNewHorizon.java
@@ -1,5 +1,31 @@
+import java.util.*;
+
 public class JavaNewHorizon {
     public static void main(String[] args) {
-        System.out.print("Hello Brand New Horizon");
+        System.out.print("Hello Brand New  Horizon");
+        printTriangle();
+    }
+    public  static void printTriangle()
+    {
+                Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows to be printed");
+        int rows = sc.nextInt();
+
+        // loop to iterate for the given number of rows
+        for (int i = 1; i <= rows; i++) {
+
+            // loop to print the number of spaces before the star
+            for (int j = rows; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            // loop to print the number of stars in each row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // for new line after printing each row
+            System.out.println();
+        }
     }
 }
